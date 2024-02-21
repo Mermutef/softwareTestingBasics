38d524a840b2b3fb4bb4703409e27809532ed991
v.kopnin <vlad.kopnen@mail.ru>
2024-02-21 16:13:43 +0300

    Все тест-кейсы

--- src/test/java/ru/ac/uniyar/testingcourse/LinearEquationSolverTest.java
+++ src/test/java/ru/ac/uniyar/testingcourse/LinearEquationSolverTest.java
@@ -2,28 +2,38 @@ package ru.ac.uniyar.testingcourse;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LinearEquationSolverTest {
     /**
-     * Проверка решения линейного уравнения в действительных числах
+     * Проверка решения линейного уравнения в действительных числах при не целом корне и параметрах
      * Дано: а = 23.59684, б = -288.166969764
      * Ожидаемый результат: -12.2121
      */
     @Test
-    void realNumberTest() {
+    void realNumberCaseWithRealParams() {
         assertThat(LinearEquationSolver.solve(23.59684, -288.166969764)).isEqualTo(-12.2121);
     }
 
     /**
-     * Проверка решения линейного уравнения в отрицательных числах
+     * Проверка решения линейного уравнения в действительных числах при не целых параметрах
+     * Дано: а = 1.3, б = 2.6
+     * Ожидаемый результат: 2
+     */
+    @Test
+    void realNumberCaseWithIntegerRoot() {
+        assertThat(LinearEquationSolver.solve(1.3, 2.6)).isEqualTo(2);
+    }
+
+    /**
+     * Проверка решения линейного уравнения в целых числах при одном отрицательном параметре
      * Дано: а = -7, б = 56
      * Ожидаемый результат: -8
      */
     @Test
-    void negativeNumberTest() {
+    void integerNumbersCaseWithNegativeRoot() {
         assertThat(LinearEquationSolver.solve(-7, 56))
-                .isNegative()
                 .isEqualTo(-8);
     }
 
@@ -33,21 +43,55 @@ public class LinearEquationSolverTest {
      * Ожидаемый результат: 4
      */
     @Test
-    void positiveNumberTest() {
-        assertThat(LinearEquationSolver.solve(-18, 72))
-                .isNegative()
-                .isEqualTo(-4);
+    void positiveNumbersCaseWithNegativeRoot() {
+        assertThat(LinearEquationSolver.solve(18, 72))
+                .isEqualTo(4);
+    }
+
+    /**
+     * Проверка решения линейного уравнения в действительных числах при корне - бесконечной дроби (спецификация не полна)
+     * Дано: а = 3, б = 1
+     * Ожидаемый результат: 0.333 +-eps
+     */
+    @Test
+    void realNumbersCaseWithInfinityRoot() {
+        assertThat(LinearEquationSolver.solve(3, 1))
+                .isCloseTo(0.333, offset(0.001));
     }
 
     /**
      * Проверка решения линейного уравнения в случае бесконечного числа решений
      * Дано: а = 0, б = 0
-     * Ожидаемый результат: бесконечность
+     * Ожидаемый результат: метод не завершит работу с исключением LinearEquationSolver.AnyNumberIsRootException
      */
     @Test
-    void infinityRootsTest() {
-        assertThat(LinearEquationSolver.solve(0, 0))
-                .isEqualTo(Double.POSITIVE_INFINITY);
+    void infinityRootsCase() {
+        /**assertThrows(LinearEquationSolver.AnyNumberIsRootException.class,
+                () -> LinearEquationSolver.solve(0, 0));*/
+        assertThatThrownBy(() -> LinearEquationSolver.solve(0, 0))
+                .isInstanceOf(LinearEquationSolver.AnyNumberIsRootException.class);
+    }
+
+    /**
+     * Проверка решения линейного уравнения в случае отсутствия решений
+     * Дано: а = 0, б = 1
+     * Ожидаемый результат: null
+     */
+    @Test
+    void noRootsCase() {
+        assertThat(LinearEquationSolver.solve(0, 1))
+                .isEqualTo(null);
+    }
+
+    /**
+     * Проверка решения линейного уравнения в случае нулевого корня
+     * Дано: а = 1, б = 0
+     * Ожидаемый результат: 0
+     */
+    @Test
+    void zeroRootCase() {
+        assertThat(LinearEquationSolver.solve(1, 0))
+                .isEqualTo(0);
     }
 
 }
